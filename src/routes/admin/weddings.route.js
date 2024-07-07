2528d8637633d15f3300009d490023b4796da567
Nikita Minchenkov <sunty1111@gmail.com>
2024-07-07 15:14:24 +0300

    Test api

--- src/routes/admin/weddings.route.js
+++ src/routes/admin/weddings.route.js
@@ -5,6 +5,7 @@ exports.initWeddingRoute = async (app, WeddingModel) => {
   const controllers = await initWeddingControllers(WeddingModel);
 
   app.get("/weddings", controllers.getAllWeddings);
+  app.get("/api/weddings", controllers.getAllWeddings);
   app.get("/weddings/:id", controllers.getWedding);
   app.post("/weddings", controllers.createWedding);
   app.put("/weddings/:id", controllers.updateWedding);
