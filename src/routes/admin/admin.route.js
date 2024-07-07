aa74ad279ed8ca310b7b5329c9903206a7e8dafe
Nikita Minchenkov <sunty1111@gmail.com>
2024-07-07 13:15:46 +0300

    Add api to routes

--- src/routes/admin/admin.route.js
+++ src/routes/admin/admin.route.js
@@ -4,5 +4,5 @@ const initAdminControllers =
 exports.initAdminRoute = async (app, models) => {
   const { getAllData } = await initAdminControllers(models);
 
-  app.get("/weddings/:id/settings", getAllData);
+  app.get("/api/weddings/:id/settings", getAllData);
 };
--- src/routes/landing/groupAnswer.route.js
+++ src/routes/landing/groupAnswer.route.js
@@ -4,5 +4,5 @@ const initGroupAnswerControllers =
 exports.initGroupAnswerRoute = async (app, GroupAnswer) => {
   const { setGroupAnswer } = await initGroupAnswerControllers(GroupAnswer);
 
-  app.put("/groups/:id/answers/:answerId", setGroupAnswer);
+  app.put("/api/groups/:id/answers/:answerId", setGroupAnswer);
 };
